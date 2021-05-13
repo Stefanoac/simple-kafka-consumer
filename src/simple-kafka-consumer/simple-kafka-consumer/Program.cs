911d7250f2502cb9715530ecb2bf5b54b6ac2ce7
Stéfano Caberlon <stefanocaberlon5@gmail.com>
2021-05-13 11:41:04 -0300

    Add simple kafka consumer

---
+++ src/simple-kafka-consumer/simple-kafka-consumer/Program.cs
@@ -0,0 +1,56 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace simple_kafka_consumer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "127.0.0.1:9092",
+                // Note: The AutoOffsetReset property determines the start offset in the event
+                // there are not yet any committed offsets for the consumer group for the
+                // topic/partitions of interest. By default, offsets are committed
+                // automatically, so in this example, consumption will only start from the
+                // earliest message in the topic 'my-topic' the first time you run the program.
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using var c = new ConsumerBuilder<Ignore, string>(conf).Build();
+
+            {
+                c.Subscribe("test-topic");
+
+                var cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = c.Consume(cts.Token);
+                            Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    c.Close();
+                }
+            }
+        }
+    }
+}
